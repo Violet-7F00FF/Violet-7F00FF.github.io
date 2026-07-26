123f66d1b5ba01044155335b286cf9dd732d22e7
공뇽 <sangin.navy@gmail.com>
2026-07-27 01:02:54 +0900

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@
    Supabase → Settings → Data API 의 Project URL
    두 값을 따옴표 사이에 붙여넣으세요.                        */
 
-window.SUPABASE_URL = 'https://mqnuwwrfqfzenzxthygo.supabase.co/rest/v1/';
+window.SUPABASE_URL = 'https://mqnuwwrfqfzenzxthygo.supabase.co';
 window.SUPABASE_KEY = 'sb_publishable_kIYwQNK2-Ml0H2mx1hwgNA_gZLr4oA_';
 
 
